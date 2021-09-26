456eb8736e4aad4ffd787ef469bb5c74270654a0
rasalaslu <73087132+rasalaslu@users.noreply.github.com>
2021-09-25 22:23:55 -0500

    9/25

---
+++ ApplicationCore/Entities/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    [Table("Review")]
+    public class Review
+    {
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+        [Column(TypeName = "decimal(3, 2)")]
+        public decimal Rating { get; set; }
+        [MaxLength(4096)]
+        public string ReviewText { get; set; }
+    }
+}
